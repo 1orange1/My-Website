cab3a5f66936586696dd78419b060efdad65e9a7
1orange1 <yangyang88827@163.com>
2018-03-14 22:41:42 +0800

    hj

--- js/index.js
+++ js/index.js
@@ -1,5 +1,63 @@
+
+
+
+
 //welcome轮播图
 
+$(function () {
+    var nowIndex = 0;//当前显示的下标
+    var timer;
+    var $lis = $("#nav li");
+    $lis.on("mouseover", function(){
+        nowIndex = $(this).index();
+        changeImg();
+    });
+    $("#prev").on("click", function(){
+        nowIndex--;
+        if(nowIndex == -1){
+            nowIndex = $lis.length - 1;
+        }
+        changeImg();
+    });
+    $("#next").on("click", function(){
+        nowIndex++;
+        if(nowIndex == $lis.length){
+            nowIndex = 0;
+        }
+        changeImg();
+    });
+    play();
+
+    $("#welcome").hover(function(){
+        clearInterval(timer);
+    }, function(){
+        play();
+    });
+
+    function play(){
+        timer = setInterval(function(){
+            $("#next").trigger("click");
+        }, 1000);
+    }
+    function changeImg(){
+        $lis.eq(nowIndex).addClass("selected").siblings().removeClass("selected");
+        $("#imgs img").eq(nowIndex).addClass("selected").siblings().removeClass("selected");
+    }
+});
+
+
+
+
+//右侧导航
+$(".go-welcome").on("click",function () {
+    $("html,body").stop().animate({scrollTop:$("#welcome").offset().top-74},1000);
+});
+$(".go-project").on("click",function () {
+    $("html,body").stop().animate({scrollTop:$("#project").offset().top-74},1000);
+});
+$(".go-ability").on("click",function () {
+    $("html,body").stop().animate({scrollTop:$("#ability").offset().top-74},1000);
+});
 
 
 
@@ -19,11 +77,17 @@
 
 
 
-
-
-
-
-
+//回到顶部
+var oGoTop = $("#goTop");
+oGoTop.on('click',function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var timer = setInterval(function () {
+        window.scrollTo(0, scrollTop-=100);
+        if(scrollTop <= 0){
+            clearInterval(timer);
+        }
+    }, 30);
+});
 
 
 
